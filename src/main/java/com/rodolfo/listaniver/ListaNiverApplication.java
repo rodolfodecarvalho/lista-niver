88f7e329e342558ab2391ead7ef5cc42a97917c2
Rodolfo Guerra <rodolfoguerraster@gmail.com>
2025-07-13 12:53:44 -0300

    start project

---
+++ src/main/java/com/rodolfo/listaniver/ListaNiverApplication.java
@@ -0,0 +1,13 @@
+package com.rodolfo.listaniver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaNiverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListaNiverApplication.class, args);
+    }
+
+}
