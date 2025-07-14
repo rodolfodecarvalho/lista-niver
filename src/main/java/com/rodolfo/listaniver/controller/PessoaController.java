98cf1557eadccfe34f0776e3d71291415c6a4514
Rodolfo Guerra <rodolfoguerraster@gmail.com>
2025-07-14 03:41:51 -0300

    ajustes

--- src/main/java/com/rodolfo/listaniver/controller/PessoaController.java
+++ src/main/java/com/rodolfo/listaniver/controller/PessoaController.java
@@ -8,6 +8,7 @@ import com.rodolfo.listaniver.service.PessoaService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -62,7 +63,7 @@ public class PessoaController implements PessoaSwaggerController {
 
     @GetMapping("/buscar")
     public ResponseEntity<List<PessoaOutputDTO>> buscarPorNome(@RequestParam String nome) {
-        log.info("Requisição para buscar pessoas por nome: {}", nome);
+        log.info("Requisição para buscar pessoas por nome: {}", StringUtils.replaceChars(nome, "\r\n", "__"));
         List<PessoaOutputDTO> result = service.buscarPorNome(nome);
         return ResponseEntity.ok(result);
     }
